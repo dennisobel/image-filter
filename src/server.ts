0ccd793c17e5b739ce885d03aea10b72e07c85d5
dennis.obel <dennis.obel@gmail.com>
2022-05-27 12:44:31 +0300

    bug fixes

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,9 @@ import path from "path"
   // Use the body parser middleware for post requests
   app.use(bodyParser.json());
 
-  app.get(`/filteredimage?image_url={{URL}}`, async (req, res) => {
-    function validURL(str: any) {
+  app.get(`/filteredimage/`, async (req: express.Request, res: express.Response) => {
+    const { image_url } = req.query;
+    function urlCheck(str: string) {
       var pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
         '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
         '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
@@ -27,48 +28,23 @@ import path from "path"
       return !!pattern.test(str);
     }
 
-    if (validURL(req.params.URL) == true) {
-      filterImageFromURL(req.params.URL).then((response) => {
-        res.send(response)
-      }).then(() => {
-        // delete files
-        const directoryPath = path.join(__dirname, 'tmp')
-        fs.readdir(directoryPath, (err, files) => {
-          if (err) {
-            return console.log('Unable to scan directory: ' + err);
-          }
-
-          deleteLocalFiles(files)
-        })
+    if (urlCheck(image_url) == true) {
+      const response: string = await filterImageFromURL(image_url);
+      res.status(200).sendFile(response, {}, function (error) {
+        if (error) {
+          res.status(500).send({ message: "file request failed" });
+        }
+        else {
+          deleteLocalFiles([response]);
+        }
       })
     } else {
-      res.send("Invalid URL")
+      res.status(400).send("Invalid URL")
     }
-
   })
 
-  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
-  // GET /filteredimage?image_url={{URL}}
-  // endpoint to filter an image from a public url.
-  // IT SHOULD
-  //    1
-  //    1. validate the image_url query
-  //    2. call filterImageFromURL(image_url) to filter the image
-  //    3. send the resulting file in the response
-  //    4. deletes any files on the server on finish of the response
-  // QUERY PARAMATERS
-  //    image_url: URL of a publicly accessible image
-  // RETURNS
-  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
-
-  /**************************************************************************** */
-
-  //! END @TODO1
-
-  // Root Endpoint
-  // Displays a simple message to the user
   app.get("/", async (req, res) => {
-    res.send("try GET /filteredimage?image_url={{}}")
+    res.status(200).send("try GET /filteredimage?image_url={{}}")
   });
 
 
--- src/util/util.ts
+++ src/util/util.ts
@@ -11,18 +11,28 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
+      const photo: any = await Jimp.read(inputURL);
+      const outpath: any =
         "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+      console.log("inside filter", outpath)
       await photo
         .resize(256, 256) // resize
         .quality(60) // set JPEG quality
         .greyscale() // set greyscale
-        .write(__dirname + outpath, (img) => {
+        .write(__dirname + outpath, (img: any) => {
           resolve(__dirname + outpath);
         });
+
     } catch (error) {
-      reject(error);
+      const photo: any = await Jimp.read(inputURL);
+      const outpath: any = '/tmp/filtered.' + Math.floor(Math.random() * 2000) + '.jpg'
+      await photo
+        .resize(256, 256) // resize
+        .quality(60) // set JPEG quality
+        .greyscale() // set greyscale
+        .write(__dirname + outpath, (img: any) => {
+          resolve(__dirname + outpath);
+        });
     }
   });
 }
